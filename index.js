e6e5d292368beaeeeecf757e89d5d9b9539b7315
padiernas <padierna.suarez@gmail.com>
2023-02-10 16:34:36 -0500

    Actulizacion del juego, ganador, bloqueo de juego, retorno

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 let character = "x";
 let player = document.querySelector("p");
+let boxes = document.querySelectorAll(".box div");
+let conditions = [
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+];
+let gameOver = false;
+
+function triqui() {
+  for (let [a, b, c] of conditions) {
+    if (
+      boxes[a].innerHTML === character &&
+      boxes[b].innerHTML === character &&
+      boxes[c].innerHTML === character
+    ) {
+      player.innerHTML = `player ${character} has won!`;
+      gameOver = true;
+      return true;
+    }
+  }
+  return false;
+}
 
 document.querySelectorAll(".box div").forEach((element) => {
   element.addEventListener("click", () => {
-    if (element.innerHTML === "") {
+    if (element.innerHTML === "" && !gameOver) {
       element.innerHTML = character;
-      character = character === "x" ? "o" : "x";
-      player.innerHTML = `player ${character} turn`;
+      if (!triqui()) {
+        character = character === "x" ? "o" : "x";
+        player.innerHTML = `player ${character} turn`;
+      }
     }
   });
 });
-
-
 
 function reset() {
   document
     .querySelectorAll(".box div")
     .forEach((element) => (element.innerHTML = ""));
+  gameOver = false;
+  player.innerHTML = `player ${character} turn`;
 }
